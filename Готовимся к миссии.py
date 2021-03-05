88ed074e251933f2d272b61c326e0416dd7b0c86
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-03-05 20:04:45 +0500

    second

--- Готовимся к миссии.py
+++ Готовимся к миссии.py
@@ -1,13 +1,25 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
     return render_template("base.html", title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    if "инженер" in prof or "строитель" in prof:
+        type_prof = "Научные симуляторы"
+        image_dir = url_for('static', filename='img/train.jpg')
+    else:
+        type_prof = "Тренажеры"
+        image_dir = url_for('static', filename='img/train1.jpg')
+
+    return render_template("trainings.html", title=type_prof, type_prof=type_prof,
+                           image_dir=image_dir)
 
 
 if __name__ == '__main__':
